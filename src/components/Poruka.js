74b94e2dc662070dddf2b807ba44201abdd126d8
Petra Vucic <pvucic@pmfst.hr>
2021-10-25 17:53:05 +0200

    Moj domaci

---
+++ src/components/Poruka.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Poruka = ({poruka, promjenaVaznosti}) => {
+   const oznaka = poruka.vazno
+  ? 'označi kao nevažno' : 'označi kao važno'
+  
+  return (
+    <li className="poruka">
+      {poruka.sadrzaj}
+      <button onClick={promjenaVaznosti}>{oznaka}</button>
+    </li>
+  )
+}
+
+export default Poruka
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import "./index.css"
+
+const poruke = [
+  {
+    id: 1,
+    sadrzaj: 'HTML je jednostavan',
+    datum: '2019-05-30T17:30:31.098Z',
+    vazno: true
+  },
+  {
+    id: 2,
+    sadrzaj: 'React koristi JSX sintaksu',
+    datum: '2019-05-30T18:39:34.091Z',
+    vazno: false
+  },
+  {
+    id: 3,
+    sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+    datum: '2019-05-30T19:20:14.298Z',
+    vazno: true
+  }
+]
+//const App = (props) => {
+  //const { poruke } = props
+
+  //return (
+    //<div>
+      //<h1>Poruke</h1>
+      //<ul>
+        //{poruke.map(p =>
+          //<Poruka key={p.id} poruka={p} />
+        //)}
+      //</ul>
+    //</div>
+  //)
+//}
+
+ReactDOM.render(<App poruke={poruke} />,document.getElementById('root'))
